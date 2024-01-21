3004d49a5987347890f288aa324d08339550ad6d
Ark-Aak <arknights_aak@outlook.com>
2024-01-21 17:17:47 +0800

    2024/01/21 17:17

--- 2024-01-19/codechef.cpp
+++ 2024-01-19/codechef.cpp
@@ -30,91 +30,51 @@ void print(_Tp x) {
 	putchar(x % 10 + '0');
 }
 
-const int MAXN = 105;
-int n, a[MAXN][3], ans;
-int T, sel[MAXN], lst[3], cnt[3];
+const int MAXN = 1005;
+const int B = 250;
+int n, a[MAXN][3];
+int f[3][B + 5][B + 5], g[3][B + 5][B + 5];
+int T;
 
-void dfs(int step) {
-	if (step > n) {
-		memset(cnt, 0, sizeof cnt);
-		memset(lst, 0, sizeof lst);
-		int sum = 0;
-		rep (i, 1, n) {
-			cnt[sel[i]] += a[i][sel[i]];
-			lst[sel[i]] = i;
-			rep (j, 0, 2) cnt[j] -= i - lst[j], cnt[j] = max(cnt[j], 0ll);
-		}
-		rep (i, 0, 2) sum += cnt[i];
-		ans = max(ans, sum);
-		return;
-	}
-	rep (i, 0, 2) {
-		sel[step] = i;
-		dfs(step + 1);
-	}
-}
+void cmax(int &x, int y) { if (x < y) x = y; }
 
 void solve() {
 	n = read();
+	const int TB = min(n, B);
 	rep (i, 1, n) a[i][0] = read(), a[i][1] = read(), a[i][2] = read();
-	if (n <= 10) {
-		ans = 0;
-		memset(cnt, 0, sizeof cnt);
-		memset(lst, 0, sizeof lst);
-		dfs(1);
-		print(ans), putchar(10);
-	}
-	else {
-		ans = 0;
-		memset(cnt, 0, sizeof cnt);
-		memset(lst, 0, sizeof lst);
-		rep (i, 1, n) {
-			int tlst[3], tcnt[3], tans = 0, fans = 0, pans = -1;
-			rep (i, 0, 2) tlst[i] = lst[i], tcnt[i] = cnt[i];
-			tlst[0] = i, tcnt[0] += a[i][0];
-			tcnt[1] = max(0ll, tcnt[1] - (i - tlst[1]));
-			tcnt[2] = max(0ll, tcnt[2] - (i - tlst[2]));
-			rep (i, 0, 2) tans += tcnt[i];
-			if (tans > fans) pans = 0, fans = tans;
-			tans = 0;
-			rep (i, 0, 2) tlst[i] = lst[i], tcnt[i] = cnt[i];
-			tlst[1] = i, tcnt[1] += a[i][1];
-			tcnt[0] = max(0ll, tcnt[0] - (i - tlst[0]));
-			tcnt[2] = max(0ll, tcnt[2] - (i - tlst[2]));
-			rep (i, 0, 2) tans += tcnt[i];
-			if (tans > fans) pans = 1, fans = tans;
-			tans = 0;
-			rep (i, 0, 2) tlst[i] = lst[i], tcnt[i] = cnt[i];
-			tlst[2] = i, tcnt[2] += a[i][2];
-			tcnt[1] = max(0ll, tcnt[1] - (i - tlst[1]));
-			tcnt[0] = max(0ll, tcnt[0] - (i - tlst[0]));
-			rep (i, 0, 2) tans += tcnt[i];
-			if (tans > fans) pans = 2;
-			tans = 0;
-			if (pans == 0) {
-				lst[0] = i, cnt[0] += a[i][0];
-				cnt[1] = max(0ll, cnt[1] - (i - lst[1]));
-				cnt[2] = max(0ll, cnt[2] - (i - lst[2]));
-				rep (i, 0, 2) tans += cnt[i];
-				ans = tans;
-			}
-			if (pans == 1) {
-				lst[1] = i, cnt[1] += a[i][1];
-				cnt[0] = max(0ll, cnt[0] - (i - lst[0]));
-				cnt[2] = max(0ll, cnt[2] - (i - lst[2]));
-				rep (i, 0, 2) tans += cnt[i];
-				ans = tans;
-			}
-			if (pans == 2) {
-				lst[2] = i, cnt[2] += a[i][2];
-				cnt[1] = max(0ll, cnt[1] - (i - lst[1]));
-				cnt[0] = max(0ll, cnt[0] - (i - lst[0]));
-				rep (i, 0, 2) tans += cnt[i];
-				ans = tans;
+	rep (i, 1, n) {
+		memcpy(g, f, sizeof f);
+		memset(f, 0, sizeof f);
+		rep (j, max(0ll, i - TB), i) rep (k, max(0ll, i - TB), i) {
+			int u = i - j ? i - j + 1 : 0, v = i - k ? i - k + 1 : 0;
+			cmax(f[0][u][v], g[0][i - j][i - k] + a[i][0]);
+			cmax(f[1][1][v], g[0][i - j][i - k] + a[i][1]);
+			cmax(f[2][1][u], g[0][i - j][i - k] + a[i][2]);
+			cmax(f[0][1][v], g[1][i - j][i - k] + a[i][0]);
+			cmax(f[1][u][v], g[1][i - j][i - k] + a[i][1]);
+			cmax(f[2][u][1], g[1][i - j][i - k] + a[i][2]);
+			cmax(f[0][v][1], g[2][i - j][i - k] + a[i][0]);
+			cmax(f[1][u][1], g[2][i - j][i - k] + a[i][1]);
+			cmax(f[2][u][v], g[2][i - j][i - k] + a[i][2]);
+		}
+		rep (j, 0, 2) {
+			rep (k, 0, TB) {
+				rep (l, 0, TB) {
+					f[j][k][l] -= k + l;
+				}
 			}
 		}
-		print(ans), putchar(10);
 	}
+	int ans = 0;
+	rep (j, 0, 2) {
+		rep (k, 0, TB) {
+			rep (l, 0, TB) {
+				ans = max(ans, f[j][k][l]);
+			}
+		}
+	}
+	print(ans), putchar(10);
+	memset(f, 0, sizeof f);
 }
 
 signed main() {
